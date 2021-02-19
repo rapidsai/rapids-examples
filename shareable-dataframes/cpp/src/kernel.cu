ae9c20a100239a0c0ae17240e17a3851ffaa766c
Jeremy Dyer <jdye64@gmail.com>
2021-02-19 17:51:18 -0500

    add examples for table in and table out operations

--- shareable-dataframes/cpp/src/kernel.cu
+++ shareable-dataframes/cpp/src/kernel.cu
@@ -11,3 +11,22 @@ __global__ void kernel_tenth_mm_to_inches(cudf::mutable_column_device_view colum
         column.element<int64_t>(i) = column.element<int64_t>(i) * (1/10) * mm_to_inches;
     }
 }
+
+__global__ void kernel_tenth_mm_to_inches_table(cudf::mutable_table_device_view tbl, int column_index)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < tbl.num_rows()) {
+        cudf::mutable_column_device_view column = tbl.column(column_index);
+        column.element<int64_t>(i) = column.element<int64_t>(i) * (1/10) * mm_to_inches;
+    }
+}
+
+__global__ void kernel_tenth_mm_to_inches_in_and_out(cudf::table_device_view in_tbl, cudf::mutable_table_device_view out_tbl, int column_index)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < in_tbl.num_rows()) {
+        cudf::mutable_column_device_view column = in_tbl.column(column_index);
+        cudf::mutable_column_device_view column out_col = out_tbl.column(0);
+        out_column.element<int64_t>(i) = column.element<int64_t>(i) * (1/10) * mm_to_inches;
+    }
+}
