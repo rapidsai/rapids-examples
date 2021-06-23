57a31fae13cb112a71675661126c7659bf28eae0
Jeremy Dyer <jdye64@gmail.com>
2021-06-23 17:15:16 -0400

    first try

--- shareable-dataframes/cpp/src/kernel_wrapper.cu
+++ shareable-dataframes/cpp/src/kernel_wrapper.cu
@@ -14,16 +14,26 @@
  * limitations under the License.
  */
 
- #include "kernel.cu"
- #include "kernel_wrapper.hpp"
- #include <assert.h>
- #include <cstdio>
- #include <cudf/column/column_device_view.cuh>
+#include "cudf/column/column_view.hpp"
+#include "kernel_wrapper.hpp"
+#include <assert.h>
+#include <cstdio>
+#include <cudf/copying.hpp>
+#include <cudf/column/column_device_view.cuh>
  
  
  CudfWrapper::CudfWrapper(cudf::mutable_table_view table_view) {
    mtv = table_view;
  }
+
+__global__ void kernel_tenth_mm_to_inches(cudf::mutable_column_device_view &column)
+{
+    printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < column.size()) {
+        column.element<float>(i) = column.element<float>(i) * 25.4;
+    }
+}
  
  void CudfWrapper::tenth_mm_to_inches(int column_index) {
  
@@ -31,17 +41,16 @@
    printf("kernel_wrapper.cu # of columns: %lu\n", mtv.num_columns());
    printf("kernel_wrapper.cu # of rows: %lu\n", mtv.num_rows());
  
-  //  //print out column dtypes for example sake only, not required,
-  //  std::for_each( mtv.begin(), mtv.end(), [](auto c) {
-  //    printf("%d type=%d, ptr=%p\n", c, static_cast<int>(c.type().id()), c.data<char>() );
-  //  });
- 
    std::unique_ptr<cudf::mutable_column_device_view, std::function<void(cudf::mutable_column_device_view*)>> 
-   mutable_device_column = cudf::mutable_column_device_view::create(mtv.column(column_index));
+          mutable_device_column = cudf::mutable_column_device_view::create(mtv.column(column_index));
+
+  std::unique_ptr<cudf::scalar> sc = cudf::get_element(mtv.column(column_index), 0);
+   //printf("Value before kernel: %d\n", mutable_device_column->get_element(0));
  
    // Invoke the Kernel to convert tenth_mm -> inches
    kernel_tenth_mm_to_inches<<<(mtv.num_rows()+255)/256, 256>>>(*mutable_device_column);
-   cudaDeviceSynchronize();
+   cudaError_t err = cudaStreamSynchronize(0);
+   printf("cudaStreamSynchronize Response = %d\n", (int)err);
  }
  
  CudfWrapper::~CudfWrapper() {
