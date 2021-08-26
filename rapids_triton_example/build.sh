3fcab423ff43ae9b889be264bbdf8c31545923bb
Vibhu Jawa <vibhujawa@gmail.com>
2021-08-26 13:59:10 -0700

    added path to download the model

--- rapids_triton_example/build.sh
+++ rapids_triton_example/build.sh
@@ -1,2 +1,4 @@
-docker build -t rapidstriton .
+### dowbload model 
+wget https://rapidsai-data.s3.amazonaws.com/community-examples/sentiment_analysis_model/model.pt -P models/sentiment_analysis_model/1/
 
+docker build -t rapidstriton .
