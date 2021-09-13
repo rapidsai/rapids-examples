9ab3665f0b8341c567dfea67688233d520d4063a
Shane Ding <shane200195@gmail.com>
2021-09-13 22:50:36 +0000

    added basic data transformation example

--- libcuml-libcudf-groupby/src/process_csv.cpp
+++ libcuml-libcudf-groupby/src/process_csv.cpp
@@ -1,6 +1,9 @@
 #include <cudf/aggregation.hpp>
 #include <cudf/groupby.hpp>
 #include <cudf/io/csv.hpp>
+#include <cudf/copying.hpp>
+#include <cudf/reshape.hpp>
+#include <cudf/transpose.hpp>
 #include <cudf/table/table.hpp>
 
 #include <memory>
@@ -24,22 +27,34 @@ void write_csv(cudf::table_view const& tbl_view, std::string const& file_path)
   cudf::io::write_csv(options);
 }
 
-std::unique_ptr<cudf::table> cuml_regression_on_groupby(cudf::table_view stock_info_table)
+
+std::unique_ptr<cudf::column> generate_grouped_arr(cudf::table_view values, cudf::size_type start, cudf::size_type end)
+{
+  auto sliced_table = cudf::slice(values, {start, end}).front();
+  auto [_, transposed_table] = cudf::transpose(sliced_table);
+
+  return cudf::interleave_columns(transposed_table);
+}
+
+std::unique_ptr<cudf::table> cuml_regression_on_groupby(cudf::table_view input_table)
 {
   // Schema: | Timestamp | Name | X | Y
-  auto keys = cudf::table_view{{stock_info_table.column(1)}};  // name
+  auto keys = cudf::table_view{{input_table.column(0)}};  // name
 
   // Compute the average of each company's closing price with entire column
   cudf::groupby::groupby grpby_obj(keys);
 
-  cudf::groupby::groupby::groups gb_groups = grpby_obj.get_groups(stock_info_table.select({2,3}));
-  return std::make_unique<cudf::table>(stock_info_table.select({2,3}));
+  cudf::groupby::groupby::groups gb_groups = grpby_obj.get_groups(input_table.select({1,2}));
+
+  auto values_view = (gb_groups.values)->view();
+  auto interleaved = generate_grouped_arr(values_view, 0, 3);
+  return std::make_unique<cudf::table>(cudf::table_view({interleaved->view()}).select({0}));
 }
 
 int main(int argc, char** argv)
 {
   // Read data
-  auto sample_table = read_csv("test.csv");
+  auto sample_table = read_csv("test2.csv");
 
   // Process
   auto result = cuml_regression_on_groupby(*sample_table.tbl);
