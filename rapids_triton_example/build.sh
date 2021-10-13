6acd249ffe29e82f99ccbe5e2c882a80af86a02c
Vibhu Jawa <vibhujawa@gmail.com>
2021-10-12 20:54:29 -0700

    working onnx

--- rapids_triton_example/build.sh
+++ rapids_triton_example/build.sh
@@ -1,5 +1,5 @@
 ### dowbload model 
-wget https://rapidsai-data.s3.amazonaws.com/community-examples/sentiment_analysis_model/model.pt -P models/sentiment_analysis_model/1/
+# wget https://rapidsai-data.s3.amazonaws.com/community-examples/sentiment_analysis_model/model.pt -P models/sentiment_analysis_model/1/
 ### add a empty folder for ensemble model 
 mkdir -p models/end_to_end_model/1
 
