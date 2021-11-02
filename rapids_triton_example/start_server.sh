e0469cdde443023c33c333b6dfc81f3300b0952d
Vibhu Jawa <vibhujawa@gmail.com>
2021-11-02 16:06:18 -0700

    21.10-update

--- rapids_triton_example/start_server.sh
+++ rapids_triton_example/start_server.sh
@@ -1,3 +1,3 @@
 rm -rf models/.ipynb_checkpoints/
 
-docker run --rm  --gpus='1' --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p 8000:8000 -p 8001:8001 -p 8002:8002 -v ${PWD}/models:/models -it rapidstriton tritonserver --model-repository /models
+docker run --rm  --gpus='1' --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p 8000:8000 -p 8001:8001 -p 8002:8002 -it rapidstriton tritonserver --model-repository models
