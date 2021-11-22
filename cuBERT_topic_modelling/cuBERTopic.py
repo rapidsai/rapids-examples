747b8cf2302fc732e47a648c250853783aac7a59
Mayank Anand <m7anand@uwaterloo.ca>
2021-11-22 16:55:50 +0000

    clean up commented code

--- cuBERT_topic_modelling/cuBERTopic.py
+++ cuBERT_topic_modelling/cuBERTopic.py
@@ -137,53 +137,6 @@ class gpu_BERTopic:
         tf_idf, count = self.new_c_tf_idf(docs_df, len(docs_df))
         return tf_idf, count, docs_per_topics_topics, docs_df
 
-    # def top_n_idx_sparse(self, matrix, n):
-    #     """Return indices of top n values in each row of a sparse matrix
-    #     Retrieved from:
-    #         https://stackoverflow.com/questions/49207275/finding-the-top-n-values-in-a-row-of-a-scipy-sparse-matrix
-    #     Args:
-    #         matrix: The sparse matrix from which to get the
-    #         top n indices per row
-    #         n: The number of highest values to extract from each row
-    #     Returns:
-    #         indices: The top n indices per row
-    #     """
-    #     top_n_idx = []
-    #     mat_inptr_np_ar = matrix.indptr.get()
-    #     le_np = mat_inptr_np_ar[:-1]
-    #     ri_np = mat_inptr_np_ar[1:]
-
-    #     for le, ri in zip(le_np, ri_np):
-    #         le = le.item()
-    #         ri = ri.item()
-    #         n_row_pick = min(n, ri - le)
-    #         top_n_idx.append(
-    #             matrix.indices[
-    #                 le + cp.argpartition(
-    #                     matrix.data[le:ri], -n_row_pick
-    #                 )[-n_row_pick:]
-    #             ]
-    #         )
-    #     return cp.array(top_n_idx)
-
-    # def top_n_values_sparse(self, matrix, indices):
-    #     """Return the top n values for each row in a sparse matrix
-    #     Args:
-    #         matrix: The sparse matrix from which to get the top n
-    #         indices per row
-    #         indices: The top n indices per row
-    #     Returns:
-    #         top_values: The top n scores per row
-    #     """
-    #     top_values = []
-    #     for row, values in enumerate(indices):
-    #         scores = cp.array(
-    #             [matrix[row, value] if value is not None
-    #              else 0 for value in values]
-    #         )
-    #         top_values.append(scores)
-    #     return cp.array(top_values)
-
     # Topic representation
     def extract_top_n_words_per_topic(
         self, tf_idf, count, docs_per_topics_topics, mmr_flag=False, n=30
